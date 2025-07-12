1dd570c6f79ff2943bb35987e55b0e788589cd88
A-DBN <adabin@hotmail.fr>
2025-07-12 16:46:25 +0200

    chore(api): configure axios baseURL using environment variables

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.API_URL || "http://localhost:8000",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
     withCredentials: false
 });
+
+console.log(process.env.REACT_APP_API_URL);
+console.log("Axios instance created with base URL:", axiosInstance.defaults.baseURL);
 
 export default axiosInstance;
