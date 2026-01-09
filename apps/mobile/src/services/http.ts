d3dddd8a829dac167b9d242f72b587c839bf550a
A-DBN <adabin@hotmail.fr>
2026-01-09 22:24:08 +0100

    feat: improve http errors and capture telemetry

--- apps/mobile/src/services/http.ts
+++ apps/mobile/src/services/http.ts
@@ -1,28 +1,75 @@
 import { env } from "../config/env";
+import { captureError } from "../telemetry/telemetry";
 
 export class HttpError extends Error {
   status: number;
+  method: string;
+  path: string;
+  url: string;
+  bodyText?: string;
 
-  constructor(message: string, status: number) {
-    super(message);
-    this.status = status;
+  constructor(opts: {
+    message: string;
+    status: number;
+    method: string;
+    path: string;
+    url: string;
+    bodyText?: string;
+  }) {
+    super(opts.message);
+    this.status = opts.status;
+    this.method = opts.method;
+    this.path = opts.path;
+    this.url = opts.url;
+    this.bodyText = opts.bodyText;
   }
 }
 
 async function request<T>(path: string, init?: RequestInit): Promise<T> {
   const url = `${env.apiUrl}${path}`;
-  const res = await fetch(url, {
-    ...init,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      ...(init?.headers ?? {}),
-    },
-  });
+  const method = (init?.method ?? "GET").toUpperCase();
+
+  let res: Response;
+  try {
+    res = await fetch(url, {
+      ...init,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        ...(init?.headers ?? {}),
+      },
+    });
+  } catch (e) {
+    const err = new HttpError({
+      message: "NETWORK_ERROR",
+      status: 0,
+      method,
+      path,
+      url,
+    });
+    captureError(e, { where: "http.fetch", method, path, url });
+    captureError(err, { where: "http.fetch", method, path, url });
+    throw err;
+  }
 
   if (!res.ok) {
     const text = await res.text().catch(() => "");
-    throw new HttpError(text || `HTTP ${res.status}`, res.status);
+    const err = new HttpError({
+      message: `HTTP_${res.status}`,
+      status: res.status,
+      method,
+      path,
+      url,
+      bodyText: text || undefined,
+    });
+    captureError(err, {
+      where: "http.response",
+      method,
+      path,
+      url,
+      status: res.status,
+    });
+    throw err;
   }
 
   return (await res.json()) as T;
